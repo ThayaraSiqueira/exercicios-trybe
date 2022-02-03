2afaa53492d6012b8f6000efe397f44d7a96b52d
Thayara Siqueira <thay.siqu@gmail.com>
2022-02-03 17:04:40 -0600

    melhora o codigo do ex 8 e 9

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
@@ -1,6 +1,7 @@
-let countTo25 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+let countTo25 = [];
 
-
-for (let index = 0; index < countTo25.length; index += 1) {
-    console.log(countTo25[index]);
+for (let index = 1; index <= 25; index += 1) {
+    countTo25.push(index);
 }
+
+console.log(countTo25);
