41ecab1007898ef4ea680c5b6f186f4acd2c9506
Thayara Siqueira <thay.siqu@gmail.com>
2022-04-05 16:19:50 -0600

    add exercises 10.1

---
+++ front-end/bloco-1-introducao-a-react/dia-1-hello-world-no-react/nome-app/src/Content.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const conteudos = [
+    {
+      conteudo: 'High Order Functions',
+      bloco: 8,
+      status: 'Aprendido'
+    },
+    {
+      conteudo: 'Composicao de Componentes',
+      bloco: 11,
+      status: 'Aprendendo',
+    },
+    {
+      conteudo: 'Composicao de Estados',
+      bloco: 12,
+      status: 'Aprenderei'
+    },
+    {
+      conteudo: 'Redux',
+      bloco: 16,
+      status: 'Aprenderei'
+    },
+  ];
+
+class Content extends React.Component {
+    render () {
+        return <div className="content">{conteudos.map((e) => (
+            <div key={e.conteudo}>
+                <h3>{`O conteúdo é: ${e.conteudo}`}</h3>
+                <h3>{`Status: ${e.status}`}</h3>
+                <h3>{`Bloco: ${e.bloco}`}</h3>
+            </div>
+            ))}
+        </div>
+    }
+}
+
+export default Content;
